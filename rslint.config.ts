2555cc9db3dff882834ca849d5b814ee8fbd950c
neverland <chenjiahan.jait@bytedance.com>
2026-05-09 14:20:09 +0800

    chore: update rslint configuration

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,6 +1,3 @@
-import { defineConfig, ts } from '@rslint/core';
+import { defineConfig, js, ts } from '@rslint/core';
 
-export default defineConfig([
-  { ignores: ['**/dist/**'] },
-  ts.configs.recommended,
-]);
+export default defineConfig([js.configs.recommended, ts.configs.recommended]);
